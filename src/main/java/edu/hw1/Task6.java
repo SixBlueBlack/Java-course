e3b98367372877086d4f68c4612d32c6a8d3b1a2
SBB <me.mashkin7@gmail.com>
2023-10-15 23:13:27 +0500

    Task6 fix

--- src/main/java/edu/hw1/Task6.java
+++ src/main/java/edu/hw1/Task6.java
@@ -8,15 +8,15 @@ public class Task6 {
     private static final int KAPREKARS_ROUTINE = 6174;
     private static final int NUMBER_LENGTH = 4;
     private static final int BOUNDARY_VALUE = 1000;
+    private static final int IDENTICAL_DENOMINATOR_VALUE = 1111;
 
     private Task6() {
     }
 
     public static int countK(int number) {
         String numberString = String.valueOf(number);
-        String firstSymbol = String.valueOf(numberString.charAt(0));
         if (number == BOUNDARY_VALUE || numberString.length() != NUMBER_LENGTH
-            || numberString.length() - numberString.replace(firstSymbol, "").length() == NUMBER_LENGTH) {
+            || number % IDENTICAL_DENOMINATOR_VALUE == 0) {
             return -1;
         }
         int increasedOrderSorted =
