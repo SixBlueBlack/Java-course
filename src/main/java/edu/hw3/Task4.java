909b1cfdc29e8cd136974412bb46268310d5f5d6
SBB <me.mashkin7@gmail.com>
2023-10-29 17:22:53 +0500

    Task4 checkstyle

--- src/main/java/edu/hw3/Task4.java
+++ src/main/java/edu/hw3/Task4.java
@@ -1,26 +1,28 @@
 package edu.hw3;
 
+import java.util.Map;
 import java.util.TreeMap;
+import static java.util.Map.entry;
 
 public class Task4 {
     private Task4() {
     }
 
-    private final static TreeMap<Integer, String> MAP = new TreeMap<>() {{
-        put(1000, "M");
-        put(900, "CM");
-        put(500, "D");
-        put(400, "CD");
-        put(100, "C");
-        put(90, "XC");
-        put(50, "L");
-        put(40, "XL");
-        put(10, "X");
-        put(9, "IX");
-        put(5, "V");
-        put(4, "IV");
-        put(1, "I");
-    }};
+    private final static TreeMap<Integer, String> MAP = new TreeMap<>(Map.ofEntries(
+        entry(1000, "M"),
+        entry(900, "CM"),
+        entry(500, "D"),
+        entry(400, "CD"),
+        entry(100, "C"),
+        entry(90, "XC"),
+        entry(50, "L"),
+        entry(40, "XL"),
+        entry(10, "X"),
+        entry(9, "IX"),
+        entry(5, "V"),
+        entry(4, "IV"),
+        entry(1, "I")
+    ));
 
     public static String convertToRoman(int arabicNumber) {
         int closestLessNumber = MAP.floorKey(arabicNumber);
